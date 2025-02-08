b8ee55140d7d4e4bd6fd9de840446e6a3b85727f
HanbeiZhou <112411827+HanbeiZhou@users.noreply.github.com>
2025-02-07 22:53:47 -0500

    Add files via upload
    commited asset and project

---
+++ Assets/PlayerShooting.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerShooting : MonoBehaviour
+{
+    public GameObject prefab;
+    public GameObject shootingPoint;
+    private void Start()
+    {
+        
+    }
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse0)) {
+            GameObject clone = Instantiate(prefab);
+            clone.transform.position = shootingPoint.transform.position;
+            clone.transform.rotation = shootingPoint.transform.rotation;
+            
+        }
+        
+    }
+}
